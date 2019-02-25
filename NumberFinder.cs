97dcd88afdc070a0cd7d79c2262b2c1a24eece57
avocadokek <47501889+avocadokek@users.noreply.github.com>
2019-02-25 16:56:09 +0200

    Add files via upload

--- NumberFinder.cs
+++ NumberFinder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace PadawansTask6
 {
@@ -6,8 +7,38 @@ namespace PadawansTask6
     {
         public static int? NextBiggerThan(int number)
         {
-            // put your code here
+            string number1 = number.ToString();
+
+
+            StringBuilder sb = new StringBuilder(number1);
+            for (int i = number1.Length - 1; i > 0; i--)
+            {
+                if (sb[i] > sb[i - 1])
+                {
+                    char temp = sb[i];
+                    sb[i] = sb[i - 1];
+                    sb[i - 1] = temp;
+
+
+                }
+                else { return null; }
+                number1 = sb.ToString();
+                number = Convert.ToInt32(number1);
+                break;
+
+            }
+
+
+
+            return number;
             throw new NotImplementedException();
+            throw new ArgumentNullException();
+            throw new ArgumentException();
+        }
+        public static void Main()
+        {
+            var result = NextBiggerThan(76);
+            Console.WriteLine(result);
         }
     }
 }
